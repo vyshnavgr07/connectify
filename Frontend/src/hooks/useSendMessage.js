ec69244170073b26628d7a932f187ee280c1a11a
vyshnavgr07 <vyshnavgr07@gmail.com>
2024-10-13 13:40:54 +0530

    message sending problem solved

--- Frontend/src/hooks/useSendMessage.js
+++ Frontend/src/hooks/useSendMessage.js
@@ -8,13 +8,15 @@ const {messages,setMessages,selectedConversation}=useConversation();
 
 
 const sendMessage=async(data)=>{
+    console.log(data)
 setLoading(true)
 try {
 const response=await api.post(`messages/send/${selectedConversation._id}`,data)
-console.log(response,"ress")
+console.log(response,"response from send message")
 if(response.status==201){
-    setMessages([...messages,response.data.message])
+    setMessages([...messages,data])
 }
+console.log(messages,"from send message")
 } catch (error) {
     console.log(error,"error")
 }finally{
