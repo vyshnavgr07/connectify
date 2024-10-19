9d0323051d3eab8e7ddc6280f9df6a2a4ca9274d
vyshnavgr07 <vyshnavgr07@gmail.com>
2024-10-19 19:02:26 +0530

    updatejson

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <Routes>
  
       <Route path='/' element={authUser?<Home/>:<Navigate to="login"/>}/>
-      <Route path='/login' element={authUser?<Navigate to="/"/>:<Login/>}/>
+      <Route path='/login' element={authUser?<Navigate to="/"/>:<Login/>}/> 
       <Route path='/signup' element={authUser?<Navigate to="/"/>:<Signup/>}/>
       </Routes>
     <Toaster/>  
